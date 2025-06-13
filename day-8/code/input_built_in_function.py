47edad907992b9655d261350a0c32b0c1d31319c
phani-sl <slphani335@gmail.com>
2025-06-13 14:00:28 +0530

    built in functions

---
+++ day-8/code/input_built_in_function.py
@@ -0,0 +1,24 @@
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+sum_result = int(a) + int(b)
+print("The sum of the two numbers is: " + str(sum_result))
+
+
+def addition(i, j):
+    print("i + j = ", i + j)
+
+k_str = input("Enter values : ")
+h_str = input()
+
+k_int = int(k_str)
+h_int = int(h_str)
+addition(k_int, h_int)
+
+
+number = input("Enter a number: ")
+print("You entered:", number)
+
--- day-8/code/type_conversion_built_in_functions.py
+++ day-8/code/type_conversion_built_in_functions.py
@@ -1,11 +1,35 @@
+# converting string to integer
 
-print(bool("False"))  # => Result =>  True
+a = int("100")
+print(a, type(a))
 
-print(bool(False))  # => False
+# converting string to float
+b = float("100.5")
+print(b, type(b))
 
+# converting string to boolean
+print(bool("z"))
 
-print(bool("0"))  # True
-print(bool(0))  # False
+# converting float to integer
+x = int(222.2)
+print(x, type(x))
+
+# converting float to string
+y = str(222.2)
+print(y, type(y))
+
+# converting integer to string
+z = str(222)    
+print(z, type(z))
+
+# converting boolean to string
+w = str(True)
+print(w, type(w))
+
+# converting boolean to integer
+v = int(False)
+print(v, type(v))
+
 
 
 
