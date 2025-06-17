8bcdc0a615e178a20b7b27ddeee2ce93bb2e44ad
phani-sl <slphani335@gmail.com>
2025-06-17 18:03:56 +0530

    set datatype

---
+++ day-10/code/set_datatype.py
@@ -0,0 +1,55 @@
+
+
+empty_set = set()
+print(empty_set)
+
+marks = {1, 2, 3, 4, 5}
+print(marks)
+print(type(marks))
+
+marks.add(7)
+print("after adding 7", marks)
+
+marks.add(7)
+print("after adding duplicate 7", marks)
+
+marks.copy()
+print("after copying marks", marks)
+
+marks.clear()
+print("after clearing marks", marks)
+
+set_1 = {1, 2, 3, 4, 5, 6}
+print("set_1:", set_1)
+set_2 = {2, 5, 7, 8, 9}
+print("set_2:", set_2)
+
+set_1.difference(set_2)
+print("set_1.difference(set_2):", set_1.difference(set_2))
+set_2.difference(set_1)
+print("set_2.difference(set_1):", set_2.difference(set_1))
+
+set_1.difference_update(set_2)
+print("set_1 after difference_update with set_2:", set_1)
+set_2.difference_update(set_1)
+print("set_2 after difference_update with set_1:", set_2)
+
+set_1.intersection(set_2)
+print("set_1.intersection(set_2):", set_1.intersection(set_2))
+set_2.intersection(set_1)
+print("set_2.intersection(set_1):", set_2.intersection(set_1))
+
+set_1.intersection_update(set_2)
+print("set_1 after intersection_update with set_2:", set_1)
+set_2.intersection_update(set_1)
+print("set_2 after intersection_update with set_1:", set_2)
+
+#set_1.remove()
+#print("set_1 after removing 2:", set_1)
+
+set_1.discard(3)
+print("set_1 after discarding 3:", set_1)
+
+
+
+
