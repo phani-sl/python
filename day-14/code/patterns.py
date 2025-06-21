1cd49327fb494fd7221d7c11d24cc4035c17e3e1
phani-sl <slphani335@gmail.com>
2025-06-21 16:40:57 +0530

    patterns

---
+++ day-14/code/patterns.py
@@ -0,0 +1,19 @@
+# right half pyramid
+
+for row in range(1, 6):
+    print("* " * row)
+
+print("=======")
+
+for column in range (1, 6):
+    print("* " * column)
+
+print("=======")
+
+print("* " * 5)
+print("* " * 4)
+print("* " * 3)
+print("* " * 2)
+print("* " * 1)
+
+print("========")
