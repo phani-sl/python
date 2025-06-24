b26b884d076e5a8b6533b1d39487f0e32f4ed811
phani-sl <slphani335@gmail.com>
2025-06-24 17:32:31 +0530

    patterns

---
+++ day-15/code/number_triangular.py
@@ -0,0 +1,6 @@
+rows = 4
+for i in range (1, rows+1):
+    print(" " * (rows-i), end="")
+    for j in range (i):
+        print(i, end=" ")
+        print()
---
+++ day-15/code/square_hollow_pattern.py
@@ -0,0 +1,6 @@
+rows = 6
+columns = 5
+for i in range(0, rows+1):
+    print("* " * rows, end="")
+    
+    print()
