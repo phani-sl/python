5c765beb27946285cc7f0cdeb4c8dcfa2a76e5b3
phani-sl <slphani335@gmail.com>
2025-06-26 14:34:55 +0530

    python basics

---
+++ day-4/code/python_basics.py
@@ -0,0 +1,38 @@
+
+"""
+Declaration:
+{variable_name}:{data_type}
+a:int
+b:float
+c:string
+d:bool
+
+
+Assigining:
+{variable_name} = {value}
+name = "Phani"
+age = 20
+cgpa = 9.4
+bool = True
+
+"""
+
+
+a = 5
+print(a)
+print(type(a))
+
+b = 3.14
+print(b)
+print(type(b))
+
+c = "Hello, World!"
+print(c)
+print(type(c))
+
+d = True
+print(d)
+print(type(d))
+
+
+
