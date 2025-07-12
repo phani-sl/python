de86925b4a469d9aa199eac7f4e71a438a791857
phani-sl <slphani335@gmail.com>
2025-07-12 09:49:29 +0530

    python basics

---
+++ day-2/code/basics.py
@@ -0,0 +1,22 @@
+
+age : int
+age = 20
+print("age is =",age)
+print(type(age))
+
+salary : float
+salary = 29292.67
+print("salary is =",salary)
+print(type(salary))
+
+name : str
+name = "phani"
+print("name is =",name)
+print(type(name))
+
+status : bool
+currently_active = True
+print("active status =",currently_active)
+print(type(currently_active))
+
+
