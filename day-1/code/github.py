4dfd1f8b79293baca47c73c655a624af07a64768
phani-sl <slphani335@gmail.com>
2025-07-12 09:37:41 +0530

    github & repository

---
+++ day-1/code/github.py
@@ -0,0 +1,27 @@
+
+# github & commands
+
+# staging
+# commit
+# push or publish
+
+# staging
+'''
+to add all files - git add .
+to add specific file - git add {specific file name, file name 2, ...........}
+
+'''
+
+# commit
+'''
+git commit -m "msg"
+
+'''
+
+# push
+'''
+git push
+
+'''
+
+
