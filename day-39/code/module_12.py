1f623b710973e679d98e0e32ec51035285be7d55
phani-sl <slphani335@gmail.com>
2025-08-22 11:16:05 +0530

    Modules

---
+++ day-39/code/module_12.py
@@ -0,0 +1,15 @@
+
+
+import module_1
+import module_2
+
+def main():
+    a = 10
+    b = 5
+
+    print("Add:", module_1.add(a, b))
+    print("Subtract:", module_1.subtract(a, b))
+    print("Multiply:", module_2.multiply(a, b))
+    print("Divide:", module_2.divide(a, b))
+
+main()
---
+++ day-39/code/module_2.py
@@ -0,0 +1,9 @@
+
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
